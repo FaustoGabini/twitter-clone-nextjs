964cac3090feac3d53b7f3f3342d871fdad3a374
Fausto Gabini <fausgabini@gmail.com>
2021-02-18 12:54:43 -0300

    Agregamos styles jsx al button

--- components/Button/index.js
+++ components/Button/index.js
@@ -1,4 +1,5 @@
 import { colors } from "../../styles/theme";
+
 export default function Button({ children, onClick }) {
   return (
     <>
@@ -6,15 +7,21 @@ export default function Button({ children, onClick }) {
       <style jsx>
         {`
           button {
+            align-items: center;
             background: ${colors.black};
             color: #fff;
             border: 0;
             cursor: pointer;
             border-radius: 9999px;
             font-size: 16px;
+            display: flex;
             font-weight: 800;
             padding: 8px 24px;
             transition: opacity 0.3s ease;
+          }
+
+          button :global(svg) {
+            margin-right: 8px;
           }
 
           button:hover {
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head"; /* Utilidad para cambiar todo lo de adentro del he
 import AppLayout from "../components/AppLayout";
 import { colors } from "../styles/theme";
 import Button from "../components/Button";
+import GitHub from "../components/Icon/GitHub";
 export default function Home() {
   return (
     <>
@@ -19,7 +20,10 @@ export default function Home() {
             with developers
           </h2>
           <div>
-            <Button>Login with GitHub</Button>
+            <Button>
+              <GitHub fill="#fff" width={24} height={24} />
+              Login with GitHub
+            </Button>
           </div>
         </section>
       </AppLayout>
@@ -36,12 +40,12 @@ export default function Home() {
         }
 
         h1 {
-          color: ${colors.primary};
+          color: ${colors.secondary};
           font-weight: 800;
           margin-bottom: 16px;
         }
         h2 {
-          color: ${colors.secondary};
+          color: ${colors.primary};
           font-size: 21px;
           margin: 0;
         }
